0d78ad9a8f170510a7c16c3b15d81d4b41372742
MC <553515788@qq.com>
2023-03-24 22:52:51 +0800

    mineflayer Bata

---
+++ mineflayer/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__author__ = "C418____11 <553515788@qq.com>"
+__version__ = "0.0.1"
+version = __version__.split('.')
+
+import sys
+
+print("Python version {0}.{1}.{2}".format(*sys.version_info))
+print("MineFlayer version {0}.{1}.{2}".format(*version))
+
+__all__ = ["auto_eat", "mineflayer"]
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
